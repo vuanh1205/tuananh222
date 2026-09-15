281ead0016a00c81367dfd13261062db817c8a7e
tuan-anh84426644 <vuongdog822@gmail.com.com>
2026-09-15 15:56:52 +0700

    lab1

--- lab1.js
+++ lab1.js
@@ -2,4 +2,9 @@ console.log ("đây là lab1");
 let name="vutuananh";
 let age=21;
 let adress="nghệ an";
-let isStudent = "là sinh viên"
+let isStudent = "là sinh viên";
+
+console.log("tên tôi là: ", name);
+console.log("tuổi: ", age);
+console.log("địa chỉ: ", adress);
+console.log("trangk thái: ", isStudent);
