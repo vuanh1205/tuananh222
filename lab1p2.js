13439a4f2301690d9fb9e2e6e407bc7cb8792371
tuan-anh84426644 <vuongdog822@gmail.com.com>
2026-09-17 15:20:07 +0700

    lab1p2

---
+++ lab1p2.js
@@ -0,0 +1,25 @@
+//bai2
+let age =20;
+console.log(age);
+
+age = 21;
+console.log(age);
+
+//bài 3
+const a = 10;
+const b = 5;
+console.log("tổng: ", a+b);
+console.log("hiệu: ", a-b);
+console.log("tích: ", a*b);
+console.log("thương: ", a/b);
+
+//bài 4
+function sayHello(name){
+    console.log("xin chào " + name);
+}
+
+sayHello("An");
+sayHello("Anh");
+sayHello("Bình");
+sayHello("chi");
+sayHello("Dũng");
