8ecb2939c1202f255653d330d09eb1c7272445c5
Vũ Văn Tuấn Anh <101194061+vuanh1205@users.noreply.github.com>
2026-09-17 15:29:17 +0700

    lab1p2

--- lab1.js
+++ lab1.js
@@ -8,3 +8,28 @@ console.log("tên tôi là: ", name);
 console.log("tuổi: ", age);
 console.log("địa chỉ: ", adress);
 console.log("trangk thái: ", isStudent);
+//bai2
+let age =20;
+console.log(age);
+
+age = 21;
+console.log(age);
+
+//bài 3
+const a = 10;
+const b = 5;
+console.log("tổng: ", a+b);
+console.log("hiệu: ", a-b);
+console.log("tích: ", a*b);
+console.log("thương: ", a/b);
+
+//bài 4
+function sayHello(name){
+    console.log("xin chào " + name);
+}
+
+sayHello("An");
+sayHello("Anh");
+sayHello("Bình");
+sayHello("chi");
+sayHello("Dũng");
